fd1d49d76681c45387826de23d5c69207bb72b3a
YOGESH05030 <yogesh202006@gmail.com>
2026-05-12 15:10:23 +0530

    Update header to remove last name

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Lalitha S V</h1>
+          <h1>Lalitha </h1>
           <p>
           <code>CI CD</code>
           </p>
